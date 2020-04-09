262ad776a228bac710f579ae3f228ac2778f46f7
Sasha Sofin <avlasof@mail.ru>
2020-04-09 17:26:38 +0300

    Личный проект: генератор публикаций

---
+++ src/service/constants.js
@@ -0,0 +1,16 @@
+'use strict';
+module.exports = {
+  CLI_DEFAULT_COMMAND: `--help`,
+  CLI_ARGS_INDEX: 2,
+  FILE_SIZE_MIN: 1,
+  FILE_SIZE_MAX: 1000,
+  FILE_NAME: `mocks.json`,
+  ANNOUNCE_MAX: 1,
+  FULL_TEXT_MAX: 5,
+  CREATED_DATE_MIN: 3,
+  MONTH_MILLISECONDS: 2592000000,
+  EXIT_CODE: {
+    SUCCESS: 0,
+    ERROR: 1
+  }
+};
---
+++ src/service/utils.js
@@ -0,0 +1,20 @@
+'use strict';
+
+const getRandomIntInclusive = (min, max) => {
+  min = Math.ceil(min);
+  max = Math.floor(max);
+  return Math.floor(Math.random() * (max - min + 1)) + min;
+};
+
+const shuffleArray = (a) => {
+  for (let i = a.length - 1; i > 0; i--) {
+    const j = Math.floor(Math.random() * (i + 1));
+    [a[i], a[j]] = [a[j], a[i]];
+  }
+  return a;
+};
+
+module.exports = {
+  getRandomIntInclusive,
+  shuffleArray,
+};
